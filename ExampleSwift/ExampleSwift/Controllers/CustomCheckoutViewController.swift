9178df4808c3ee7fac0692eaa6fc964905dbbb8d
André Cocuroci <andre.cocuroci@mercadolivre.com>
2022-01-11 17:58:52 -0300

    (Added)[JIRA-2998] - Added text field to configure product id

--- ExampleSwift/ExampleSwift/Controllers/CustomCheckoutViewController.swift
+++ ExampleSwift/ExampleSwift/Controllers/CustomCheckoutViewController.swift
@@ -15,6 +15,7 @@ class CustomCheckoutViewController: UIViewController {
     @IBOutlet private var publicKeyTextField: UITextField!
     @IBOutlet private var preferenceIdTextField: UITextField!
     @IBOutlet private var accessTokenTextField: UITextField!
+    @IBOutlet private var productIdTextField: UITextField!
     @IBOutlet private var oneTapSwitch: UISwitch!
 
     @IBAction private func iniciarCheckout(_ sender: Any) {
@@ -25,7 +26,7 @@ class CustomCheckoutViewController: UIViewController {
                 title: "Error",
                 message: "Complete los campos requeridos para continuar",
                 preferredStyle: .alert
-                )
+            )
             alert.addAction(UIAlertAction(title: "Ok", style: .default, handler: nil))
             present(alert, animated: true)
             return
@@ -38,28 +39,15 @@ class CustomCheckoutViewController: UIViewController {
         publicKeyTextField.text = ""
         preferenceIdTextField.text = ""
         accessTokenTextField.text = ""
+        productIdTextField.text = ""
         oneTapSwitch.setOn(true, animated: true)
     }
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        let gradient = CAGradientLayer()
-        gradient.frame = view.bounds
-        let col1 = UIColor(red: 34.0 / 255.0, green: 211 / 255.0, blue: 198 / 255.0, alpha: 1)
-        let col2 = UIColor(red: 145 / 255.0, green: 72.0 / 255.0, blue: 203 / 255.0, alpha: 1)
-        gradient.colors = [col1.cgColor, col2.cgColor]
-        view.layer.insertSublayer(gradient, at: 0)
-
-        if let path = Bundle.main.path(forResource: "Info", ofType: "plist"),
-           let infoPlist = NSDictionary(contentsOfFile: path) {
-            // Initialize values from config
-            publicKeyTextField.text = infoPlist["PX_COLLECTOR_PUBLIC_KEY"] as? String
-            accessTokenTextField.text = infoPlist["PX_PAYER_PRIVATE_KEY"] as? String
-        }
-
-        localeTextField.text = "es-AR"
-        preferenceIdTextField.text = "566676612-3bbb117b-1f3f-49c9-915d-565a785837d0"
+        setupGrandient()
+        setupKeys()
+        oneTapSwitch.addTarget(self, action: #selector(onChangeSwitchValue), for: .valueChanged)
     }
 
     private func runMercadoPagoCheckoutWithLifecycle() {
@@ -68,35 +56,74 @@ class CustomCheckoutViewController: UIViewController {
             let language = localeTextField.text else {
             return
         }
-        let builder = MercadoPagoCheckoutBuilder(publicKey: publicKey, preferenceId: preferenceId).setLanguage(language)
+
+        let builder = MercadoPagoCheckoutBuilder(
+            publicKey: publicKey,
+            preferenceId: preferenceId
+        ).setLanguage(language)
+
         if let privateKey = accessTokenTextField.text {
             builder.setPrivateKey(key: privateKey)
         }
+
         if oneTapSwitch.isOn {
             let advancedConfiguration = PXAdvancedConfiguration()
-            advancedConfiguration.expressEnabled = true
+
+            if let productId = productIdTextField.text {
+                advancedConfiguration.setProductId(id: productId)
+            }
+
             builder.setAdvancedConfiguration(config: advancedConfiguration)
         }
 
         let checkout = MercadoPagoCheckout(builder: builder)
+
         if let myNavigationController = navigationController {
             checkout.start(navigationController: myNavigationController, lifeCycleProtocol: self)
         }
+    }
+
+    private func setupGrandient() {
+        let gradient = CAGradientLayer()
+        gradient.frame = view.bounds
+        let col1 = UIColor(red: 34.0 / 255.0, green: 211 / 255.0, blue: 198 / 255.0, alpha: 1)
+        let col2 = UIColor(red: 145 / 255.0, green: 72.0 / 255.0, blue: 203 / 255.0, alpha: 1)
+        gradient.colors = [col1.cgColor, col2.cgColor]
+        view.layer.insertSublayer(gradient, at: 0)
+    }
+
+    private func setupKeys() {
+        if let path = Bundle.main.path(forResource: "Info", ofType: "plist"),
+           let infoPlist = NSDictionary(contentsOfFile: path) {
+            // Initialize values from config
+            publicKeyTextField.text = infoPlist["PX_COLLECTOR_PUBLIC_KEY"] as? String
+            accessTokenTextField.text = infoPlist["PX_PAYER_PRIVATE_KEY"] as? String
+        }
+
+        localeTextField.text = "es-AR"
+        preferenceIdTextField.text = "737302974-34e65c90-62ad-4b06-9f81-0aa08528ec53"
+        productIdTextField.text = "BJEO9NVBF6RG01IIIOTG"
+    }
+
+    @objc
+    private func onChangeSwitchValue() {
+        productIdTextField.isEnabled = oneTapSwitch.isOn
+        productIdTextField.layer.opacity = oneTapSwitch.isOn ? 1 : 0.6
     }
 }
 
 // MARK: Optional Lifecycle protocol implementation example.
 extension CustomCheckoutViewController: PXLifeCycleProtocol {
     func finishCheckout() -> ((PXResult?) -> Void)? {
-        return nil
+        nil
     }
 
     func cancelCheckout() -> (() -> Void)? {
-        return nil
+        nil
     }
 
     func changePaymentMethodTapped() -> (() -> Void)? {
-        return { () in
+        { () in
             print("px - changePaymentMethodTapped")
         }
     }
@@ -112,10 +139,7 @@ extension CustomCheckoutViewController: UITextFieldDelegate {
         shouldChangeCharactersIn range: NSRange,
         replacementString string: String
     ) -> Bool {
-        if string == " " {
-            return false
-        }
-        return true
+        string == " " ? false : true
     }
 
     func textFieldDidEndEditing(_ textField: UITextField) {
