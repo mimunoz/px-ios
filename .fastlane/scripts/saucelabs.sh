eddf2e89c4adf8596ca4d8036d50360a70c268ca
matheusctMeli <matheus.mteixeira@mercadolivre.com>
2022-02-24 09:22:09 -0300

    update ipa file path (#2671)

--- .fastlane/scripts/saucelabs.sh
+++ .fastlane/scripts/saucelabs.sh
@@ -1,5 +1,5 @@
 fileId=$(curl -sb -H -u "$SAUCELABS_USERNAME:$SAUCELABS_ACCESS_KEY" --location \
 --request POST 'https://api.us-west-1.saucelabs.com/v1/storage/upload' \
---form 'payload=@"../../ExampleSwift/build/ExampleSwift.ipa"' \
+--form 'payload=@"../../build/ExampleSwift.ipa"' \
 --form 'name="ExampleSwift.ipa"' | json item.id) 
 envman add --key SAUCELABS_FILE_ID_UPLOADED --value $fileId
