8a722e762d1cbacf3378532a829ea784e76f0806
Gisela Ramos <gisela.ramos@mercadolibre.com>
2022-03-04 14:24:03 -0300

    (Added)[PXN-3306] - New payment method model support (#2677)
    * PXN-3306 Add new fields to new card model
    
    * Update CHANGELOG.md

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -4,6 +4,11 @@ All notable changes to this project will be documented in this file.
 The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.0.0/),
 and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0.html).
 
+## Unpublished
+
+### Added
+- New payment method model support
+
 ## v4.55.1
 
 ### Fixed
--- MercadoPagoSDK/MercadoPagoSDK/Models/OneTap/PXOneTapNewCardDto.swift
+++ MercadoPagoSDK/MercadoPagoSDK/Models/OneTap/PXOneTapNewCardDto.swift
@@ -6,6 +6,10 @@ open class PXOneTapNewCardDto: NSObject, Codable {
     let descriptionText: PXText?
     let cardFormInitType: String?
     let sheetOptions: [PXOneTapSheetOptionsDto]?
+    let deeplink: String?
+    let iconUrl: String?
+    let border: PXOneTapNewCardBorderDto?
+    let backgroundColor: String?
 
     enum CodingKeys: String, CodingKey {
         case version
@@ -13,5 +17,14 @@ open class PXOneTapNewCardDto: NSObject, Codable {
         case descriptionText = "description"
         case cardFormInitType = "card_form_init_type"
         case sheetOptions = "sheet_options"
+        case deeplink
+        case iconUrl = "icon_url"
+        case border
+        case backgroundColor = "background_color"
     }
+}
+
+open class PXOneTapNewCardBorderDto: NSObject, Codable {
+    let type: String?
+    let color: String?
 }
