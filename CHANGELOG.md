7a935d84554ef708cff9155cf01e740634201752
Ricardo d'Alambert <ricardo.couto@mercadolivre.com>
2022-05-05 10:24:40 -0300

    Release/4.60.0 (#2749)
    * Release 4.60.0
    
    * [ci deploy]
    
    * [ci deploy]
    
    * * adição de canImport para CryptoKit
    [ci deploy]
    
    Co-authored-by: Jona <jonathan.scaramal@mercadolibre.com>

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -6,6 +6,8 @@ and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0
 
 ## [Unreleased]
 
+## [4.60.0] - 2022-05-05Z
+
 ### Added
 - Added DEBIN account creation
 - Added PXPaymentMethodBehaviour
--- MercadoPagoSDK/MercadoPagoSDK/Core/Extensions/Data+Additions.swift
+++ MercadoPagoSDK/MercadoPagoSDK/Core/Extensions/Data+Additions.swift
@@ -1,15 +1,23 @@
 import Foundation
-import CommonCrypto
+#if canImport(CryptoKit)
 import CryptoKit
+#endif
+#if canImport(CommonCrypto)
+import CommonCrypto
+#endif
 
 extension Data {
     private func hexString(_ iterator: Array<UInt8>.Iterator) -> String {
         return iterator.map { String(format: "%02x", $0) }.joined()
     }
-    
+
     public var sha1: String {
         if #available(iOS 13.0, *) {
+        #if canImport(CryptoKit)
             return hexString(Insecure.SHA1.hash(data: self).makeIterator())
+            #else
+            return String()
+        #endif
         } else {
             var digest = [UInt8](repeating: 0, count: Int(CC_SHA1_DIGEST_LENGTH))
             self.withUnsafeBytes { bytes in
